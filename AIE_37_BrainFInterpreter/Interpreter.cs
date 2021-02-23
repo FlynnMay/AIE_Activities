c84cb31a1e85523d017f8a6af33eb02a5cb04813
Flynn May <s210158@students.aie.edu.au>
2021-02-23 12:53:28 +1100

    BrainF

---
+++ AIE_37_BrainFInterpreter/Interpreter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AIE_37_BrainFInterpreter
+{
+    public class Interpreter
+    {
+        public byte[] tape;
+        public int pointer;
+        public char[] input;
+
+        public Interpreter()
+        {
+            tape = new byte[30000];
+        }
+
+        public void Run(string input)
+        {
+            this.input = input.ToCharArray();
+
+            int unmatchedBracketCounter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case '>':
+                        pointer++;
+                        break;
+                    case '<':
+                        pointer--;
+                        break;
+                    case '+':
+                        tape[pointer]++;
+                        break;
+                    case '-':
+                        tape[pointer]--;
+                        break;
+                    case '.':
+                        Console.Write(Convert.ToChar(tape[pointer]));
+                        break;
+                    case ',':
+                        var key = Console.ReadKey();
+                        tape[pointer] = (byte)key.KeyChar;
+                        break;
+                    case '[':
+                        if (tape[pointer] == 0)
+                        {
+                            unmatchedBracketCounter++;
+                            while (input[i] != ']' || unmatchedBracketCounter != 0)
+                            {
+                                i++;
+
+                                if (input[i] == '[')
+                                {
+                                    unmatchedBracketCounter++;
+                                }
+                                else if (input[i] == ']')
+                                {
+                                    unmatchedBracketCounter--;
+                                }
+                            }
+                        }
+                        break;
+                    case ']':
+                        if (tape[pointer] != 0)
+                        {
+                            unmatchedBracketCounter++;
+                            while (input[i] != '[' || unmatchedBracketCounter != 0)
+                            {
+                                i--;
+                                if (input[i] == ']')
+                                {
+                                    unmatchedBracketCounter++;
+                                }
+                                else if (input[i] == '[')
+                                {
+                                    unmatchedBracketCounter--;
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ AIE_37_BrainFInterpreter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AIE_37_BrainFInterpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interpreter interpreter = new Interpreter();
+            interpreter.Run("--[--->-<-->+<]>.");
+        }
+    }
+}
